50af02df9421a2ef32242d1079107fdbc5d21d70
hezhi <hezhi@shengpaypay.com>
2019-07-05 09:43:59 +0800

    删除测试文件

--- lib/config/config.dart
+++
@@ -1,7 +0,0 @@
-const serviceUrl= 'https://www.easy-mock.com/mock/5ccba720e632d85da4a24299/api/';//此端口针对于正版用户开放，可自行fiddle获取。
-
-// const serviceUrl= 'http://baixingliangfan.cn/baixing/';
-
-const servicePath={
-  'homePageContext': serviceUrl+'wxmini/homePageContent', // 商家首页信息
-};
